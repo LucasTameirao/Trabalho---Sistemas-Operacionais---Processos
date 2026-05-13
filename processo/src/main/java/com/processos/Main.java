cdadb97359d392e8101b837815b1fd1f120d41c9
LucasTameirao <lucas.atmartins@gmail.com>
2026-05-13 14:16:44 -0300

    Corrigindo a declaração de pacote do SRTF

--- processo/src/main/java/com/processos/Main.java
+++ processo/src/main/java/com/processos/Main.java
@@ -10,6 +10,6 @@ public class Main {
         //     System.out.println(p);
         // }
         // System.out.println(lista[2]);
-        System.out.println("FCFS executado. Tempo total de execução " + FCFS.iniciarSimulacao());
+        System.out.println("FCFS executado. Tempo total de execução " + SRTF.iniciarSimulacao());
     }
 }
